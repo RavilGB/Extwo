c72ba38656b689f8e22624e6f0b459f30af249c1
Ravil <karimra@mail.ru>
2022-11-26 15:00:20 +0300

    ravil

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// Console.ReadLine(Считывание данных
+// string  строка )
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Проходите пожалуйста ");
+Console.Write( username);
