52a3aa2c520969a04571c0701743e59f7e904542
Ravil <karimra@mail.ru>
2022-11-26 16:21:54 +0300

    ifelse

---
+++ Examples/Example005_Conditionifelse/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя .");
+string username = Console.ReadLine();
+if(username.ToLower() == "ravil")
+
+    Console.WriteLine("Как, мы тебе рады Равиль!");
+else 
+    Console.Write("Hello,  bro!!!");
+    Console.WriteLine(username);
