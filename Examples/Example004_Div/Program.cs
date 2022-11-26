b76eb9e0ccdb1e0e480bcce71572aaa49a6b7bc8
Ravil <karimra@mail.ru>
2022-11-26 15:34:48 +0300

    double

---
+++ Examples/Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double numberA = 12;
+double numberB = 5;
+double result = numberA/numberB;
+Console.Write(result);
