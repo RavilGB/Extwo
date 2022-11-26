8f4d0a227fcd237a6b98331de60bbb6b2a5c11aa
Ravil <karimra@mail.ru>
2022-11-26 15:48:05 +0300

    random

--- Examples/Example003-Summ/Program.cs
+++ Examples/Example003-Summ/Program.cs
@@ -1,5 +1,7 @@
 ﻿// сложение двух чисел 
-int numberA = 7;
-int numberB = 3;
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.Write(result);
